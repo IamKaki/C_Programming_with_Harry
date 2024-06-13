c1fb6926714752f48d06c517e4518afcdd3b1678
IamKaki <rizkarize@gmail.com>
2024-06-13 11:44:45 +0300

    Getting the sum of natural numbers with a do while loop

---
+++ Chapter_4/sum_of_natural_numbers_with_do-while_loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+	int i = 1, sum = 0;
+
+	do {
+		printf("Sum of natural numbers from 1-100 is %d.\n", sum);
+	}
+	while 
+		(i <= 100);
+		i++;
+		sum += i;
+	
+
+	return 0;
+}
