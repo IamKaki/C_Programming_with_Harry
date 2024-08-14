216d4ae9cd01a3cdaf6f22fd7cf2560e99f4bf01
IamKaki <rizkarize@gmail.com>
2024-08-14 16:57:39 +0300

    a programm to get and print age of user

---
+++ secondAttempt/Practice/recordAge.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this is to get and print age of user
+ * Return: 0
+ */
+
+int main(void)
+{
+	int age;
+
+	printf("Please enter your age..\n");
+
+	scanf("%d", &age);
+
+	printf("Your age is %d\n", age);
+
+	return (0);
+}
