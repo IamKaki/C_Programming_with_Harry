7b99df292185ddf4cc18c0885931a7362d3498f3
IamKaki <rizkarize@gmail.com>
2024-08-14 18:05:44 +0300

    programm that gets area of rectangle with hard coded values

---
+++ secondAttempt/Chapter1/areaOfRectangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - finds area of rectangle
+ * Return: 0
+ **/
+
+int main(void)
+{
+	int height = 20;
+	int width = 30;
+
+	printf("The area of the rectangle is %d.\n", height * width);
+
+	return (0);
+}
---
+++ secondAttempt/Chapter1/areaOfRectangle1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - get input from user then calculate area of rectangle
+ * Return: 0
+ **/
+
+int main(void)
+{
+	int h, w;
+
+	/*prompt and read height and width*/
+	printf("Please input the height..\n");
+	scanf("%d", &h);
+
+	printf("Please input the width..\n");
+	scanf("%d", &w);
+
+	int area = h * w;
+
+	/*output the resulst of the area*/
+	printf("The area of the rectangle is %d.\n", area);
+
+	return (0);
+}
