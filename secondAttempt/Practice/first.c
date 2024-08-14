6097d4dcd9e079ba20c985ce6eb7d07356d9c56b
IamKaki <rizkarize@gmail.com>
2024-08-14 16:56:31 +0300

    My fisrt C programm with styling from betty

---
+++ secondAttempt/Practice/first.c
@@ -0,0 +1,13 @@
+/**
+ * main - programm to write who is teaching me programming in C
+ * Return: this is to return 0
+ **/
+
+#include <stdio.h>
+
+int main(void)
+	{
+	printf("Hello, I am learning programming with Harry.\n");
+
+	return (0);
+}
